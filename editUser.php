79dae8582b137abaa40d5636f124938af2c8521f
Yose4Web <yoseforweb@gmail.com>
2022-12-18 14:02:58 +0700

    edit

--- editUser.php
+++ editUser.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'connection.php';
+    require_once 'connection.php';
 
-if ($con) {
+    if($con){
         $id = $_POST['id'];
         $username = $_POST['username'];
         $password = $_POST['password'];
@@ -9,21 +9,26 @@ if ($con) {
         $noTelp = $_POST['noTelp'];
         $tglLahir = $_POST['tglLahir'];
 
-    $getData = "SELECT * FROM user WHERE id = '$id'";
+        $getData = "SELECT * FROM user WHERE id = '$id'";
 
-    if ($id != "" && $username != "" && $password != "" && $email != "" && $noTelp != "" && $tglLahir != "") {
-        $result = mysqli_query($con, $getData);
-        $rows = mysqli_num_rows($result);
-        $response = array();
+        if ($id != "" && $username != "" && $password != "" && $email != "" && $noTelp != "" && $tglLahir != "") {
+            $result = mysqli_query($con, $getData);
+            $rows = mysqli_num_rows($result);
+            $response = array();
 
-        if ($rows > 0) {
-            $update = "UPDATE `user` SET `username`='$username',`password`='$password',`email`='$email',`noTelp`='$noTelp',`tglLahir`='$tglLahir' WHERE `id`=$id";
-            $exequery = mysqli_query($con, $update);
+            if ($rows > 0) {
+                $update = "UPDATE `user` SET `username`='$username',`password`='$password',`email`='$email',`noTelp`='$noTelp',`tglLahir`='$tglLahir' WHERE `id`=$id";
+                $exequery = mysqli_query($con, $update);
 
-            if ($exequery) {
-                array_push($response, array(
-                    'status' => 'OK'
-                ));
+                if ($exequery) {
+                    array_push($response, array(
+                        'status' => 'OK'
+                    ));
+                } else {
+                    array_push($response, array(
+                        'status' => 'FAILED'
+                    ));
+                }
             } else {
                 array_push($response, array(
                     'status' => 'FAILED'
@@ -39,12 +44,7 @@ if ($con) {
             'status' => 'FAILED'
         ));
     }
-} else {
-    array_push($response, array(
-        'status' => 'FAILED'
-    ));
-}
 
-echo json_encode(array("server_response" => $response));
-mysqli_close($con);
+    echo json_encode(array("server_response" => $response));
+    mysqli_close($con);
 ?>
